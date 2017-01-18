8b7062d2a81a57c62602cf2c55c6c351fd06cddb
sameer-j <jainsameer992@gmail.com>
2017-01-18 18:11:10 +0530

    Routing1-heroes and dashboard

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -1,13 +1,43 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }	 from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component'
+import { HeroService } from './hero.service'
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [ 
+  		BrowserModule,
+  		FormsModule,
+  		RouterModule.forRoot([
+			{
+				path:'heroes',
+				component: HeroesComponent
+			},
+			{
+				path: 'dashboard',
+				component: DashboardComponent
+			},
+			{
+				path: '',
+				redirectTo: '/dashboard',
+				pathMatch: 'full'
+			}
+		]) 
+  ],
+  declarations: [ 
+  		AppComponent, 
+  		HeroDetailComponent, 
+  		HeroesComponent,
+  		DashboardComponent
+  ],
+  bootstrap:    [ AppComponent ],
+  providers: [ HeroService ]
 })
+
+
 export class AppModule { }
