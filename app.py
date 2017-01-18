5d2de86b133da405bdc8ce5c4d23b5411369333c
sameer-j <jainsameer992@gmail.com>
2017-01-18 11:40:00 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, send_from_directory, request, redirect, url_for
+import os
+import subprocess
+app = Flask(__name__)
+
+BASE_URL = os.path.abspath(os.path.dirname(__file__))
+CLIENT_APP_FOLDER = os.path.join(BASE_URL, "ClientApp")
+
+#---only for development time----
+#typescript compiler watch changes in input file and convert to js
+subprocess.Popen('tsc -w', cwd=os.path.join(CLIENT_APP_FOLDER, "app"), shell=True)
+#-----
+
+
+@app.route('/')
+def home():
+	return send_from_directory(CLIENT_APP_FOLDER, 'index.html')
+
+@app.route('/<path:filename>')
+def client_app_folder(filename):
+	return send_from_directory(CLIENT_APP_FOLDER, filename)
+
+@app.route('/app/<path:filename>')
+def client_app_app_folder(filename):
+	return send_from_directory(os.path.join(CLIENT_APP_FOLDER, "app"), filename)
+
+if __name__ == "__main__":
+    app.run(debug=True)
