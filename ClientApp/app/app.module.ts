b65dfafc1f29a5fa4ac2b3b4c19e440d745b77a0
sameer-j <jainsameer992@gmail.com>
2017-01-20 12:28:57 +0530

    Http1

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }	 from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -11,10 +11,16 @@ import { HeroService } from './hero.service'
 
 import { AppRoutingModule } from './app-routing.module'
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 @NgModule({
   imports: [ 
   		BrowserModule,
   		FormsModule,
+      HttpModule,
+      InMemoryWebApiModule.forRoot(InMemoryDataService),
   		AppRoutingModule
   ],
   declarations: [ 
---
+++ in_memory_db.py
@@ -0,0 +1,17 @@
+
+class IMDB:
+      heroes = [
+            {'id': 11, 'name': 'Mr. Sameer'},
+            {'id': 12, 'name': 'Narco'},
+            {'id': 13, 'name': 'Bombasto'},
+            {'id': 14, 'name': 'Celeritas'},
+            {'id': 15, 'name': 'Magneta'},
+            {'id': 16, 'name': 'RubberMan'},
+            {'id': 17, 'name': 'Dynama'},
+            {'id': 18, 'name': 'Dr IQ'},
+            {'id': 19, 'name': 'Magma'},
+            {'id': 20, 'name': 'Tornado'}
+          ];
+
+      def getAllHeroes():
+            return heroes
