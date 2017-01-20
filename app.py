6204de1eae4398c046656284ab7882f608b14d62
jains39 <sameer.jain@emc.com>
2017-01-20 15:47:30 +0530

    replaced angular-in-memory-api with flask rest apis and dict db

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, send_from_directory, request, redirect, url_for
+from flask import jsonify
 import os
 import subprocess
+from in_memory_db import IMDB
 app = Flask(__name__)
-
+imdb = IMDB()
 BASE_URL = os.path.abspath(os.path.dirname(__file__))
 CLIENT_APP_FOLDER = os.path.join(BASE_URL, "ClientApp")
 
@@ -11,6 +13,26 @@ CLIENT_APP_FOLDER = os.path.join(BASE_URL, "ClientApp")
 subprocess.Popen('tsc -w', cwd=os.path.join(CLIENT_APP_FOLDER, "app"), shell=True)
 #-----
 
+@app.route('/api/heroes', methods=['GET'])
+def getAllHeroes():
+	return jsonify({'data':imdb.getAllHeroes()})
+
+@app.route('/api/heroes/<int:heroId>', methods=['GET'])
+def getHero(heroId):
+	return jsonify({'data':imdb.getHero(heroId)})
+
+@app.route('/api/heroes/<int:heroId>', methods=['PUT'])
+def updateHero(heroId):
+	return jsonify({'data':imdb.updateHero(heroId, request.json)})
+
+@app.route('/api/heroes', methods=['POST'])
+def createHero():
+	return jsonify({'data':imdb.createHero(request.json)})
+
+@app.route('/api/heroes/<int:id>', methods=['DELETE'])
+def deleteHero(id):
+	imdb.deleteHero(id)
+	return 'success'
 
 @app.route('/', defaults={'path': ''}) #Catch All urls, enabling copy-paste url
 @app.route('/<path:path>') #Catch All urls, enabling copy-paste url
@@ -24,6 +46,7 @@ def client_app_folder(filename):
 @app.route('/app/<path:filename>')
 def client_app_app_folder(filename):
 	return send_from_directory(os.path.join(CLIENT_APP_FOLDER, "app"), filename)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
