315e4af2901f013422e5325e39dab37e6af9cd3c
sameer-j <jainsameer992@gmail.com>
2017-02-28 19:17:03 +0530

    solidified login, role based login

--- ClientApp/app/auth.guard.ts
+++ ClientApp/app/auth.guard.ts
@@ -1,25 +1,41 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
- 
+import { AuthenticationService } from './authentication.service'
 @Injectable()
 export class AuthGuard implements CanActivate {
-    isLoggedIn: boolean = false;
+    // isLoggedIn: boolean;
     redirectUrl: string;
 
-    constructor(private router: Router) { }
+    constructor(private router: Router,
+        private authService: AuthenticationService) { }
  
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         let url:string = state.url;
-        return this.checkLogin(url);
+        if(!this.checkLogin(route.data["role"]))
+        {
+            this.router.navigate(['/login'], { queryParams: { returnUrl: url}});
+            return false;
+        }
+        else
+            return true
     }
 
-  checkLogin(url: string): boolean {
-    if (this.isLoggedIn && localStorage.getItem('currentUser')) 
-        { return true; }
+  checkLogin(role:any): boolean {
+    let currentUser = JSON.parse(localStorage.getItem('currentUser'))
+    if(currentUser && 
+        this.authService.authenticate_user(currentUser))
+    {
+        let currentUserRole = currentUser['role'];
+        if(role != null && role != currentUserRole)
+        {
+            alert('Role Unauthorised')
+            this.authService.isLoggedIn = false;
+            return false;
+        }
 
-    //store the attempted url for redirecting
-    this.redirectUrl = url;
-    this.router.navigate(['/login']);
+        this.authService.isLoggedIn = true;
+        return true; 
+    }
     return false;
   }
 }
--- ClientApp/app/login.component.ts
+++ ClientApp/app/login.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { AuthenticationService } from './authentication.service';
 
 @Component({
@@ -9,19 +9,29 @@ import { AuthenticationService } from './authentication.service';
   styleUrls: [ '../assets/login.component.css' ] 
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit{
   message: string;
   uname: string;
   pwd: string;
+  returnUrl: string;
   constructor(
     public authService: AuthenticationService, 
-    public router: Router) {};
+    public router: Router,
+    private activeRoute: ActivatedRoute) {};
+
+  ngOnInit() {
+    //reset login status
+    this.authService.logout();
+
+    //get return url from route parameters or default to '/'
+    this.returnUrl = this.activeRoute.snapshot.queryParams['returnUrl'] || '/';
+
+  }
 
   onSubmit() {
     this.authService.login(this.uname, this.pwd).subscribe(() => {
-      if (this.authService.authguard.isLoggedIn) {
-        let redirect = this.authService.authguard.redirectUrl ? this.authService.authguard.redirectUrl : '/dashboard';
-        this.router.navigate([redirect]);
+      if (this.authService.isLoggedIn) {
+        this.router.navigate([this.returnUrl]);
       }
       else
       {
